f0725d443504cf4f90c9842c4d4b175bb0d016ef
GSantos-Code <gh.santos.corporation@gmail.com>
2021-08-09 15:37:44 -0300

    Relatorio finalizado, falta criar a interface GUI e o Resumo

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import os
 class auxtools(object):
     def __init__(self, pdfname):
         self.width= 596
+        self.fim= 0
         self.enter= 0
         self.aux= 0
         self.qp= 0
@@ -40,8 +41,11 @@ class auxtools(object):
         self.pdf.drawCentredString(self.width/2,130,"Relatório de Setup Virtual")
     def write(self,texto):
         if(self.enter == 1):
+            fcpy= self.bl2._fontname
             self.bl2= self.pdf.beginText()
             self.bl2.setTextOrigin(40,50)
+            self.bl2.setFont(fcpy,13)
+            self.enter= 0
         for line in texto:
             (x,y) = self.bl2.getCursor()
             if(self.pagebreak == 1):
@@ -55,12 +59,14 @@ class auxtools(object):
             else:
                 if(line == "<"):
                     self.bl2.setFont("Helvetica-Bold",13)
+                elif(line == "§"):
+                    pass
                 elif(line == ">"):
                     self.bl2.setFont("Helvetica",13)
                 elif(line == "%"):
                     self.bl2.textLine()
                     (x,y) = self.bl2.getCursor()
-                    if(y + 200 >= 744):
+                    if(y + 200 >= 714):
                         self.aux2= ""
                         self.enter= 1
                         self.pdf.drawText(self.bl2)
@@ -120,7 +126,7 @@ class auxtools(object):
                     self.coords[3]= y
                     self.bl2.setFillColorRGB(0,0,0)
                 else:
-                    if(y >= 744):
+                    if(y >= 714):
                         self.enter= 1
                         self.aux2= ""
                         self.pdf.drawText(self.bl2)
@@ -131,52 +137,48 @@ class auxtools(object):
         if(self.pagebreak == 1):
             self.pagebreak= 0
             self.write(self.aux2)
+            if(line == "§"):
+                return 0;
             self.pdf.drawText(self.bl2)
         if(self.enter == 0):
             self.pdf.drawText(self.bl2)
     def rodape(self):
         self.pdf.setFillColorRGB(31/255,91/255,141/255)
         self.pdf.setStrokeColorRGB(31/255,91/255,141/255)
-        self.pdf.rect(0,744,width= self.width, height= 300, fill=1)
+        self.pdf.rect(0,714,width= self.width, height= 300, fill=1)
+        self.pdf.setFillColorRGB(1,1,1)
+        self.pdf.setFont("Helvetica",11)
+        self.pdf.drawString(30,759,"0800 601 7277")
+        self.pdf.drawString(30,779,"@compasscomvoce                       /compasscomvoce")
+        self.pdf.drawString(30,799,"contato@compass3d.com.br         www.compass3d.com.br")
+        self.pdf.drawString(30,819,"Av. Carandaí, 161 - 2º andar, Funcionários / BH-MG")
+        img= ImageReader("Compass3D.png")
+        self.pdf.scale(1,-1)
+        self.pdf.drawImage(img,(self.width/5)*3 + 40,-1270, width= 168, height= 525, mask='auto', preserveAspectRatio= True, anchor="l")
     def body(self):
         self.pdf.setFont("Helvetica",13)
         self.pdf.setFillColorRGB(0,0,0)
         self.pdf.drawString(40,200, "Prezado")
         self.pdf.setFont("Helvetica-Bold",13)
         self.pdf.drawString(96,200, "Dr(a) " + self.nome)
+        self.pdf.setFont("Helvetica",13)
         x= 40
         y= 230
-        '''if(self.comment != ""):
-            self.bl1= self.pdf.beginText()
-        '''
-        texto0= "É importante tambem sadasd,samdsd ssadsad sadsadasdasdas dasdasd asd as asd asd asdas dsad  sad asde bom vc saber que terminei meudsfsdf||||||||||||||||||||||||| adasdasdasd ddas dasd as das d namoro agora vou ter que achar outra pessoa pra me fazer companhia me ajuda alguem da moral ai valeu falou ok blz okas masdmsa dmsd am msad msalientar que o caso do Matheus se trata de um caso bastante complexo então peço que reavalie as condições e veja se é isso mesmo que vc quer porque o risco do caso der um PRO é enorme, e se der e você tiver comprado um ONE teria que pagar placas extras o paciente não concordaria com tal questão e você teria que arcar com fundo da sua clinica Obrigado"
-        texto0 += "||"
-        '''    self.pdf.setFont("Helvetica",13)
-            self.bl1.setTextOrigin(int(x),int(y))
-            for line in texto0:
-                (x,y) = self.bl1.getCursor()
-                if(line == "|"):
-                    self.bl1.setTextOrigin(x,y)
-                    self.bl1.textLine("")
-                elif(x >= 530 and line == " "):
-                    self.bl1.setTextOrigin(40,y)
-                    self.bl1.textLine(line)
-                else:
-                    self.bl1.textOut(line)
-            self.pdf.drawText(self.bl1)
-            self.bl1.textLine()
-            (x,y) = self.bl1.getCursor()'''
+        texto0= ""
         x= 40
         self.bl2= self.pdf.beginText()
-        texto = "{comment}||"
+        if(texto0 == ""):
+            texto= texto0
+        else:
+            texto = "{comment}||"
         texto= texto + "O Setup Virtual para <{Paciente}> foi realizado conforme suas instruções.||Modelos virtuais anexados mostram o resultado do setup virtual e poderão ser visualizados no software gratuito 3Shape Viewer.||"
         texto= texto + "<Poderá ser necessário refinamento após o término da movimentação ortodôntica em virtude da característica de alguns movimentos dentários solicitados, da complexidade do caso, da resposta biológica de cada paciente, do uso correto e contínuo do aparelho, da condução do tratamento, entre outros.>||"
         texto= texto + "O refinamento deverá ser solicitado através do e-mail &contato@compass3d.com.br# e novos modelos (ambas as arcadas) deverão ser enviados. Se optar por realizar nova moldagem, haverá necessidade de retirada dos attachments presentes para não ocorrer distorções nos modelos em gesso. Caso a opção seja o escaneamento intraoral, não há necessidade da retirada dos attachments.||"
         texto= texto + "Para realizar a etapa inicial do presente planejamento, serão necessários:||Alinhadores Superiores: <{sup}>|Alinhadores Inferiores: <{inf}>||"
         texto= texto + "Está indicado uso de attachments para maior controle do movimento dentário, para os elementos identificados abaixo:||"
         texto= texto + "Face Vestibular: {v}|Face Lingual: {l}|Face Oclusal: {o}||"
-        texto= texto + "Os attachments deverão ser instalados com o guia correspondente para cada arcada, confeccionado em placa de acetato 0,3mm enviada para este fim, antes de iniciar o uso do primeiro alinhador OrthoAligner."
-        texto= texto + "Isto implica em mais {att} modelos prototipados (identificados como “0”) e os respectivos guias para instalação dos attachments.||Total de alinhadores: <{quant}({extenso})>.||O caso será tratado com um <OrthoAligner {pacote}>.||"
+        texto= texto + "Os attachments deverão ser instalados com o guia correspondente para cada arcada, confeccionado em placa de acetato 0,3mm enviada para este fim, antes de iniciar o uso do primeiro alinhador OrthoAligner.||"
+        texto= texto + "Isto implica em mais <{att}> modelos prototipados (identificados como “0”) e os respectivos guias para instalação dos attachments.||Total de alinhadores: <{quant}({extenso})>.||O caso será tratado com um <OrthoAligner {pacote}>.||"
         texto= texto + "Você poderá escolher o tipo de placa utilizada para o tratamento entre <Ultimate> e <FLX>.|As placas <Ultimate> são mais finas, mais confortáveis e mais resistentes a manchas.|As placas <FLX> possuem maior resistência, flexibilidade e forças mais constantes||."
         texto= texto + "Além disso, você também pode optar pelo <recorte padrão> (corte reto, 2mm acima da margem gengival) ou <recorte cervical> com contorno gengival.||"
         texto= texto + "Lembrando que a escolha das placas e do recorte deve ser baseada na experiência do profissional e em especial no caso a ser tratado.||"
@@ -187,7 +189,7 @@ class auxtools(object):
         texto= texto + "Em casos com indicação de desgastes interproximais (Stripping/IPR), a quantidade e locais a serem feitos esses desgastes estão assinalados no arquivo PDF anexo. Os desgastes interproximais deverão ser iniciados já na instalação dos primeiros alinhadores e prosseguir a taxa de 0,1mm por etapa subsequente.||"
         texto= texto + "A contenção pós tratamento deverá ser instalada após o uso do último alinhador.||"
         texto= texto + "<Informamos que caso seja solicitada a alteração do setup, iremos acrescentar mais (2) dois dias úteis no prazo de entrega do planejamento para o envio do mesmo.>||"
-        texto= texto + "<{ortodont}>|Compass3D"
+        texto= texto + "<{ortodont}>|Compass3D§"
         texto= texto.format(comment= texto0,Paciente= self.nome,sup=5,inf=5,v="-",l="-",o="-",att="dois",quant="10",extenso="dez",pacote="FULL",ortodont="Diogo Frazão")
         self.bl2.setTextOrigin(int(x),int(y))
         self.coords = [0,0,0,0]
